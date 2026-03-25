c9982dd0c854cb804df244a0e3cc2d5f38baaca1
hyunjin kim <4090mike@gmail.com>
2026-03-25 10:43:54 +0900

    fix: syntax error in app.py

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, world!'
+def my_profile():
+    return {
+        "name": "김현진",
+        "role":"초보 서버 개발자",
+        "status":"로컬 환경 통제 완료!",
+        "skills": ["Ubuntu","VS Code","Python","Flask"]
+        }
 
 if __name__ == '__main__':
     app.run(debug=True)
