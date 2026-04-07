c61ecc03f58ac76cf725172c73b310e49905f5d0
hyunjin kim <4090mike@gmail.com>
2026-04-07 14:02:21 +0900

    feat: 리스트와 for문으로 화면 자동 생성 구현

--- app.py
+++ app.py
@@ -4,17 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # 여기에 내 데이터를 만든다!
-    my_profile = {
-        "name": "김현진",
-        "age": 19,
-        "school": "종로산업정보학교",
-        "hobby": "게임",
-        "phone": "010-0000-0000",
-        "dream": "개발자"
-    }
-
-    return render_template('index.html', data=my_profile)
+    foods = ["치킨", "피자", "햄버거", "떡볶이", "라면", "초밥", "김밥"]
+    return render_template('index.html', data=foods)
 
 if __name__ == '__main__':
     app.run(debug=True)
